ab04e7b48d76919258aadfecff87f2183a5de583
Dario Santos <dario.santos@mobtelecom.com.br>
2020-01-18 21:44:53 -0300

    Finalizando a interface Web da aplicação Dev Radar - Omnistack 10

---
+++ src/components/DevItem/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+function DevItem ({ dev }) {
+    return (
+        <li className="dev-item">
+            <header>
+            <img src={dev.avatar_url} />
+            <div className="user-info">
+                <strong>{dev.name}</strong>
+                <span>{dev.techs.join(', ')}</span>
+            </div>              
+            </header>
+            <p>{dev.bio}</p>
+            <a href={`https://github.com/${dev.github_username}`} target="_blank">Acessar perfil no GitHub</a>
+        </li>
+    )
+}
+
+export default DevItem;
